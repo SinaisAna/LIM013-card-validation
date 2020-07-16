b0e64dd3cd496dc7372dc85ebca153b7e4081dc7
SinaisAna <58980145+SinaisAna@users.noreply.github.com>
2020-07-16 18:24:13 -0500

    nuevo avance

--- src/index.js
+++ src/index.js
@@ -1,3 +1,53 @@
 import validator from './validator.js';
 
-console.log(validator);
+const ingresar=document.getElementById("ingresar");
+ingresar.onclick=saludoMensaje;
+
+
+function saludoMensaje(){
+	console.log("entro mensaje");
+    let nombre=document.getElementById("nombre").value;
+    if(nombre==""){
+     obigatorioNombre.innerHTML+="<p>Campo Obligatorio!</p>";
+    }else{
+    document.getElementById('cabecera').classList.add('ocultar');
+	let empecemos=document.getElementById("ingreso");
+    empecemos.innerHTML ='<h2>!Bien ' + nombre + ' por favor ingresa tu numero de tarjeta!</h2>'+ empecemos.innerHTML;
+    //mensaje2();
+    document.getElementById("ingreso").classList.remove('ocultar');
+    const tarjeta=document.getElementById("botonValidar");
+    tarjeta.onclick=evaluacion;
+ }
+
+}
+//function mensaje2(){
+
+
+function evaluacion(){
+	console.log("holaaa");
+	let input=document.getElementById("numtarjeta").value;
+	  if(input==""){
+   obligatorioTarjeta.innerHTML+="<p>Campo Obligatorio!</p>";
+    }else{
+    let resultados=validator.isValid(input);
+	let modelosTarjeta="";
+	if(input[0]=="4"){
+      modelosTarjeta="VISA";
+}else if(input[0]=="5" && (input[1]=="1" || input[1]=="2" || input[1]=="3" || input[1]=="4")){
+  modelosTarjeta="MASTERCARD";
+   }
+	console.log(resultados);
+	let michis=validator.maskify(input);
+	//console.log(input);
+	let inputTodo=document.getElementById("total");
+	inputTodo.innerHTML+="<p>"+michis+"</p>";
+	//escribe la validacion si es valida o no
+	 if(resultados==true){
+	 	inputTodo.innerHTML+="<p>tu tarjeta es valida" + modelosTarjeta+ "</p>";
+	}else{
+		inputTodo.innerHTML+="<p>tu tarjeta no es valida</p>";
+	}
+
+  }
+} 
+//}
--- src/validator.js
+++ src/validator.js
@@ -1,15 +1,55 @@
 const validator = {
   // ...
-  validador.isValid(creditCardNumber){
-    var suma= 0;
-    for (i =0;i<creditCardNumber.length; i++){
+  isValid: function(creditCardNumber){
 
+    var suma= 0;
+    for(var i = 0;i<creditCardNumber.length; i++){
+      var digito =parseInt(creditCardNumber[i]);
+      console.log("digito",digito);
+        console.log(i);
+      if((i+1)%2!=0){
+        
+        console.log("entro");
+        digito=digito*2;
+        if(digito>9){ 
+          let digito1 = parseInt(digito/10);
+          let digito2 = digito%10;
+          digito=digito1+digito2;
+          
+        }
+        
+      }
+      suma=suma+digito;
+      console.log(suma);
+    }
+  if (suma%10==0){
+      return true;
+    } else{
+      return false;
+    } 
+  },
+
+
+maskify: function(creditCardNumber){
+  var longitudNumero=creditCardNumber.length;
+  var resultado="";
+  if(longitudNumero>4){
+    for(var i = 0;i<creditCardNumber.length; i++){
+              
+      if(i>=creditCardNumber.length-4){
+        resultado+=creditCardNumber[i];
+      }else{
+        resultado+="#";
+      }
     }
 
+  }else{
+    resultado=creditCardNumber
   }
-  validador.maskify(creditCardNumber): sting{
+  return resultado;
+}
 
-  }
-;
 
-export default validator;
+}
+ export default validator;
+
