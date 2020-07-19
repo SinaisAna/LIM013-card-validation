1e0b350d4cdeed34a72793f1fe5559409fc17ab6
SinaisAna <58980145+SinaisAna@users.noreply.github.com>
2020-07-19 13:32:37 -0500

    ultimo avance

--- README.md
+++ README.md
@@ -1,42 +1,50 @@
-# tarjeta de credito valida :technologist: 	:credit_card:
+# Tarjeta de credito valida :technologist: 	:credit_card:
 
 ##Objetivo del programa :pencil:
 
 
  El fin de este programa es poder validar tarjetas de credito, a traves del algoritmo de luhn. con el fin de que sea adcesible a todo tipo de personas posean una tarjeta dando una experiencia dinamica.
 
- algoritmo de lunh : metodo creado para la verificacion de numeros de identicicacion, como los numeros de las tarjetas decredito(visa,masterCard) o el IMEI de lostelefonos moviles.El algoritmo fucniona de la siguiente manera:
+ Algoritmo de lunh : metodo creado para la verificacion de numeros de identicicacion, como los numeros de las tarjetas de credito (visa,masterCard) o el IMEI de los telefonos moviles. El algoritmo fuciona de la siguiente manera:
 
 
- 1. dar una funcion (if(i+1)%2!=0) donde tomo los numeros impar 
+ 1.Tomo los numeros obtenidos con un array y trabajo con los indice de dicho array,  obtengo el primer numero, que con la funcion (if(i+1)%2!=0) nombro al primer numero como 1 (en primer indice del caracter en  javascript es denominado "0") donde me permite tomar los numeros impares. 
 
- 2. sumar los digitos del resultado: (ejemplo: 16 = 1 + 6 = 7) junto con los dijitos sin duplicar del numero original.
+ 2. Sumar los digitos del resultado: (ejemplo: 16 = 1 + 6 = 7) junto con los dijitos sin duplicar del numero original.
 
- 3. si el total del modulo 10 es igual a 0 (si el total termina en cero), estonces el numero es valido de acuerdo con la formula luhn, de lo contrario no es valido.
+ 3. Si el total del modulo 10 es igual a 0 (si el total termina en cero), entonces el numero es valido de acuerdo con la formula luhn, de lo contrario no es valido.
 
- ## Fotos de prototipos 
+ ## Foto de prototipo :nerd_face:
+
+ ![](imagenes/1_1.jpg)
 
  ## Fotos de los avances :grinning:
 
- ! [](img/1.jpg)
+ ![](imagenes/1.jpg)
 
- ! [](img/2.jpg)
+ ![](imagenes/2.jpg)
 
- ! [](img/3.jpg)
+ ![](imagenes/3.jpg)
 
- ! [](img/4.jpg)
-
-
-
-este empieza con una bienvenida dandote un pequeño resumen de como puedes usarla, lo importante es colocar tu nombre, de ahi te pide el numero de tarjeta que vas a verificar dandote una de las dos alternativas 
-
-tu tarjate es valida(quiere decir que si posees una tarjeta existente)
-
-tu tarjeta es invalida(colocaste un numero mal, o no existe esa tarjeta) 
+ ![](imagenes/4.jpg)
 
 
-## Freedback 
 
-mi squad me dio buen visto por los primeros bocetos mostrados solo debia darle mas contenido para asi tener mas personalidad la paguina wed, dando asi mas confianza al usuario.
+ Este empieza con una bienvenida dandote un pequeño resumen de como puedes usarla, lo importante es colocar tu nombre, de ahi te pide el numero de tarjeta que vas a verificar dandote una de las dos alternativas;
+
+ 1.Tu tarjate es valida (quiere decir que si posees una tarjeta existente).
+
+ 2.Tu tarjeta es invalida (colocaste un numero mal, o no existe esa tarjeta). 
+
+
+## Feedback :pushpin:
+
+Mi squad me dio buen visto por los primeros bocetos mostrados, solo debia darle mas contenido para asi tener mas personalidad la paguina wed, dando asi mas confianza al usuario.
 
 ## Prototipo Final :champagne:
+
+![](imagenes/5.jpg)
+
+![](imagenes/6.jpg)
+
+![](imagenes/7.jpg)
--- src/index.js
+++ src/index.js
@@ -2,52 +2,92 @@ import validator from './validator.js';
 
 const ingresar=document.getElementById("ingresar");
 ingresar.onclick=saludoMensaje;
-
+const nombre="";
 
 function saludoMensaje(){
 	console.log("entro mensaje");
     let nombre=document.getElementById("nombre").value;
     if(nombre==""){
-     obigatorioNombre.innerHTML+="<p>Campo Obligatorio!</p>";
+     	obigatorioNombre.innerHTML+="<p>Campo Obligatorio!</p>";
     }else{
-    document.getElementById('cabecera').classList.add('ocultar');
-	let empecemos=document.getElementById("ingreso");
-    empecemos.innerHTML ='<h2>!Bien ' + nombre + ' por favor ingresa tu numero de tarjeta!</h2>'+ empecemos.innerHTML;
-    //mensaje2();
-    document.getElementById("ingreso").classList.remove('ocultar');
-    const tarjeta=document.getElementById("botonValidar");
-    tarjeta.onclick=evaluacion;
- }
+	    document.getElementById('cabecera').classList.add('ocultar');
+		let empecemos=document.getElementById("innert");
+	    empecemos.innerHTML ='<h2>!Bien ' + nombre + ' por favor ingresa tu numero de tarjeta!</h2>'+ empecemos.innerHTML;
+	    //mensaje2();
+	    document.getElementById("ingreso").classList.remove('ocultar');
+	    const tarjeta=document.getElementById("botonValidar");
+	    tarjeta.onclick=evaluacion;
+	 }
 
 }
-//function mensaje2(){
 
 
 function evaluacion(){
-	console.log("holaaa");
+	
+	let nombre=document.getElementById("nombre").value;
 	let input=document.getElementById("numtarjeta").value;
-	  if(input==""){
-   obligatorioTarjeta.innerHTML+="<p>Campo Obligatorio!</p>";
-    }else{
-    let resultados=validator.isValid(input);
-	let modelosTarjeta="";
-	if(input[0]=="4"){
-      modelosTarjeta="VISA";
-}else if(input[0]=="5" && (input[1]=="1" || input[1]=="2" || input[1]=="3" || input[1]=="4")){
-  modelosTarjeta="MASTERCARD";
-   }
-	console.log(resultados);
-	let michis=validator.maskify(input);
-	//console.log(input);
-	let inputTodo=document.getElementById("total");
-	inputTodo.innerHTML+="<p>"+michis+"</p>";
-	//escribe la validacion si es valida o no
-	 if(resultados==true){
-	 	inputTodo.innerHTML+="<p>tu tarjeta es valida" + modelosTarjeta+ "</p>";
-	}else{
-		inputTodo.innerHTML+="<p>tu tarjeta no es valida</p>";
-	}
+	if(input==""){
 
-  }
+	}else{
+
+
+	    let resultados=validator.isValid(input);
+		let modelosTarjeta="";
+		console.log(resultados);
+		let michis=validator.maskify(input);
+		
+		let inputTodo=document.getElementById("total");
+		document.getElementById("numerico").classList.add('ocultar');
+		
+		//escribe la validacion si es valida o no
+		let borrarBienvenida=document.getElementById("innert").classList.add('ocultar');
+		 if(resultados==true){
+		 	if(input[0]=="4"){
+		 		document.getElementById("visa").classList.remove('ocultar');
+
+		 		document.getElementById("numero-tarjeta1").innerHTML=michis;
+		
+				document.getElementById("nombre-tarjeta1").innerHTML=nombre;
+				
+		 	}else if(input[0]=="5" && (input[1]=="1" || input[1]=="2" || input[1]=="3" || input[1]=="4")){
+				document.getElementById("master").classList.remove('ocultar');
+
+		 		document.getElementById("numero-tarjeta2").innerHTML=michis;
+
+				document.getElementById("nombre-tarjeta2").innerHTML=nombre;
+				
+		 	}
+		 	inputTodo.classList.remove('resultado-rojo');
+
+		 	inputTodo.classList.add('resultado-verde');
+
+		 	inputTodo.innerHTML+="<p>Tu tarjeta es valida</p>";
+		}else{
+            inputTodo.classList.remove('resultado-verde');
+
+		 	inputTodo.classList.add('resultado-rojo');
+
+			inputTodo.innerHTML+="<p>"+michis+"</p>";
+
+			inputTodo.innerHTML+="<p>Tu tarjeta NO es valida</p>";
+
+		}
+	const ultimo=document.getElementById("volver");
+   ultimo.onclick=volver;
+  document.getElementById("volver").classList.remove('ocultar');
+	
+}
+  
+  
+}
+function volver(){
+	document.getElementById("visa").classList.add('ocultar');
+
+	document.getElementById("master").classList.add('ocultar');
+
+    document.getElementById("numerico").classList.remove('ocultar');
+
+	let inputTodo=document.getElementById("total");
+	inputTodo.innerHTML="<p></p>";
 } 
 //}
